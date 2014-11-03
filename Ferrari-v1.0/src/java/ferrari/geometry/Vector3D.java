2d63a0d1ca6ec8051d1237609b0ba0489ecd7716
matiasneal <matias.neal@gmail.com>
2014-11-02 22:41:30 -0500

    Update and rename Vector3D to Vector3D.java

---
+++ Ferrari-v1.0/src/java/ferrari/geometry/Vector3D.java
@@ -0,0 +1,45 @@
+package ferrari.geometry;
+
+public class Vector3D {
+
+	private float x, y, z;
+
+	public Vector3D(float initX, float initY, float initZ) {
+		x = initX;
+		y = initY;
+		z = initZ;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+
+	public double DotProduct(Vector3D vector) {
+		return 0.0f;
+	}
+
+	public double CrossProduct(Vector3D vector) {
+		return 0.0f;
+	}
+
+}
