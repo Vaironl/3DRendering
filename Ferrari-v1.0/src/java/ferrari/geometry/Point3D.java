54ebe1632add6af8960ec76e8245e5e6066bd8b4
matiasneal <matias.neal@gmail.com>
2014-11-04 18:58:27 -0500

    Update Point3D.java

--- Ferrari-v1.0/src/java/ferrari/geometry/Point3D.java
+++ Ferrari-v1.0/src/java/ferrari/geometry/Point3D.java
@@ -1,42 +1,21 @@
 package ferrari.geometry;
 
-public class Point3D {
+public class Point3D
+{
+	public float x;
+	public float y;
+	public float z;
 
-	private float x, y, z;
-
-	public Point3D(float initX, float initY, float initZ) {
-		x = initX;
-		y = initY;
-		z = initZ;
-	}
-
-	public void draw(Graphics2D g2d) {
-		// cast into integers temporary
-		g2d.drawLine((int) x, (int) y, (int) x, (int) y);
-	}
-
-	public float getX() {
-		return x;
-	}
-
-	public void setX(float x) {
+	public Point3D(float x, float y, float z)
+	{
 		this.x = x;
-	}
-
-	public float getY() {
-		return y;
-	}
-
-	public void setY(float y) {
 		this.y = y;
-	}
-
-	public float getZ() {
-		return z;
-	}
-
-	public void setZ(float z) {
 		this.z = z;
 	}
-
+	
+	@Override
+	public String toString()
+	{
+		return "(" + x, ", " + y + ", " + z + ")";
+	}
 }
