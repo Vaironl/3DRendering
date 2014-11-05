3bfa132d5ba2fb06e6bb157209804f3e087c4cfb
matiasneal <matias.neal@gmail.com>
2014-11-04 19:19:24 -0500

    Update Camera.java

--- Ferrari-v1.0/src/java/ferrari/Camera.java
+++ Ferrari-v1.0/src/java/ferrari/Camera.java
@@ -1,6 +1,41 @@
 package ferrari;
 
+import ferrari.geometry.Point3D;
+import ferrari.geometry.Vector3D;
+
 public class Camera
 {
-
+	private Point3D position;
+	private Vector3D orientation;
+	
+	public Camera(Point3D pos, Vector3D ori)
+	{
+		setPosition(pos);
+		setOrientation(ori);
+	}
+	
+	public Camera()
+	{
+		this(new Point3D(0, 0, 0), new Vector3D(0, 0, 1));
+	}
+	
+	public Point3D getPosition()
+	{
+		return position;
+	}
+	
+	public Vector3D getOrientation()
+	{
+		return orientation;
+	}
+	
+	public void setPosition(Point3D pos)
+	{
+		position = pos;
+	}
+	
+	public void setOrientation(Vector3D ori)
+	{
+		orientation = ori;
+	}
 }
