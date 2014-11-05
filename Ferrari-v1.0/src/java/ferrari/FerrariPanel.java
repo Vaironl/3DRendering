5b5140d880c5868f55719bd896afd6b9023ff689
matiasneal <matias.neal@gmail.com>
2014-11-04 19:43:11 -0500

    Update FerrariPanel.java

--- Ferrari-v1.0/src/java/ferrari/FerrariPanel.java
+++ Ferrari-v1.0/src/java/ferrari/FerrariPanel.java
@@ -2,5 +2,38 @@ package ferrari;
 
 public class FerrariPanel extends javax.swing.JPanel
 {
+	private Scene scene;
+	private Camera camera;
 	
+	public FerrariPanel()
+	{
+		super();
+	}
+	
+	public FerrariPanel(Scene scn, Camera cam)
+	{
+		this();
+		setScene(scn);
+		setCamera(cam);
+	}
+	
+	public Scene getScene()
+	{
+		return scene;
+	}
+	
+	public Camera getCamera()
+	{
+		return camera;
+	}
+	
+	public void setScene(Scene scn)
+	{
+		scene = scn;
+	}
+	
+	public void setCamera(Camera cam)
+	{
+		camera = cam;
+	}
 }
