cd1bc7580f09e810938188803cb7f598cad96195
matiasneal <matias.neal@gmail.com>
2014-11-04 19:25:10 -0500

    Update Entity.java

--- Ferrari-v1.0/src/java/ferrari/Entity.java
+++ Ferrari-v1.0/src/java/ferrari/Entity.java
@@ -2,4 +2,20 @@ package ferrari;
 
 public class Entity
 {
+	private Triangle3D[] polygons;
+	
+	public Entity(Triangle[] poly)
+	{
+		setPolygons(poly);
+	}
+	
+	public Triangle3D[] getPolygons()
+	{
+		return polygons;
+	}
+	
+	public void setPolygons(Triangle3D[] poly)
+	{
+		polygons = poly;
+	}
 }
