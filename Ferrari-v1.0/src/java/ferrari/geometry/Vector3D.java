87709796e43fc415f77c41d6abe86119790b19a7
matiasneal <matias.neal@gmail.com>
2014-11-11 18:21:22 -0500

    Update Vector3D.java

--- Ferrari-v1.0/src/java/ferrari/geometry/Vector3D.java
+++ Ferrari-v1.0/src/java/ferrari/geometry/Vector3D.java
@@ -13,6 +13,34 @@ public class Vector3D
 		this.z = z;
 	}
 	
+	public double magnitude()
+	{
+		return Math.sqrt(x*x + y*y + z*z);
+	}
+	
+	public void normalize()
+	{
+		double length = magnitude();
+		
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+	
+	public double dotproduct(Vector3D other)
+	{
+		return Math.sqrt(other.x * this.x +
+				 other.y * this.y +
+				 other.z * this.z);
+	}
+	
+	public Vector3D crossproduct(Vector3D other)
+	{
+		return new Vector3D(this.y * other.z - this.z * other.y,
+				    this.z * other.x - this.x * other.z,
+				    this.x * other.y - this.y * other.x);
+	}
+	
 	@Override
 	public String toString()
 	{
