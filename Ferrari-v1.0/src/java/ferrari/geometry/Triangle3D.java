b68ba4f92a36f137f274ea450178d2af89772d0c
matiasneal <matias.neal@gmail.com>
2014-11-11 18:55:18 -0500

    Update Triangle3D.java

--- Ferrari-v1.0/src/java/ferrari/geometry/Triangle3D.java
+++ Ferrari-v1.0/src/java/ferrari/geometry/Triangle3D.java
@@ -2,9 +2,9 @@ package ferrari.geometry;
 
 public class Triangle3D
 {
-	private Point3D a;
-	private Point3D b;
-	private Point3D c;
+	public Point3D a;
+	public Point3D b;
+	public Point3D c;
 
 	public Triangle3D(Point3D a, Point3D b, Point3D c)
 	{
