81b4df0a8d125314fdd1ba772e20f78cf423e36f
Vairon <vairon@live.com>
2014-11-15 21:36:33 -0500

    Fixed typos

--- Ferrari-v1.0/src/java/ferrari/Entity.java
+++ Ferrari-v1.0/src/java/ferrari/Entity.java
@@ -2,22 +2,18 @@ package ferrari;
 
 import ferrari.geometry.Triangle3D;
 
-public class Entity
-{
+public class Entity {
 	private Triangle3D[] mesh;
-	
-	public Entity(Triangle3D[] mesh)
-	{
+
+	public Entity(Triangle3D[] mesh) {
 		setMesh(mesh);
 	}
-	
-	public Triangle3D[] getMesh()
-	{
+
+	public Triangle3D[] getMesh() {
 		return mesh;
 	}
-	
-	public void setMesh(Triangle3D[] mesh)
-	{
+
+	public void setMesh(Triangle3D[] mesh) {
 		this.mesh = mesh;
 	}
 }
