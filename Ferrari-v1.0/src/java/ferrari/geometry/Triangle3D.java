84875be9d2d7eb6438b1aaf79c10d642bddea314
Vairon <vairon@live.com>
2014-11-15 21:40:52 -0500

    Fixed typos

--- Ferrari-v1.0/src/java/ferrari/geometry/Triangle3D.java
+++ Ferrari-v1.0/src/java/ferrari/geometry/Triangle3D.java
@@ -1,13 +1,11 @@
 package ferrari.geometry;
 
-public class Triangle3D
-{
+public class Triangle3D {
 	public Point3D a;
 	public Point3D b;
 	public Point3D c;
 
-	public Triangle3D(Point3D a, Point3D b, Point3D c)
-	{
+	public Triangle3D(Point3D a, Point3D b, Point3D c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
