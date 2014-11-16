105be09f729ac279f3a4e07248c74f24b4d7598b
Vairon <vairon@live.com>
2014-11-15 21:40:31 -0500

    Fixed Typos

--- Ferrari-v1.0/src/java/ferrari/geometry/Point3D.java
+++ Ferrari-v1.0/src/java/ferrari/geometry/Point3D.java
@@ -1,37 +1,31 @@
 package ferrari.geometry;
 
-public class Point3D
-{
+public class Point3D {
 	public float x;
 	public float y;
 	public float z;
 
-	public Point3D(float x, float y, float z)
-	{
+	public Point3D(float x, float y, float z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	
-	public double distanceTo(Point3D other)
-	{
+
+	public double distanceTo(Point3D other) {
 		float dx = other.x - this.x;
 		float dy = other.y - this.y;
 		float dz = other.z - this.z;
-		
-		return Math.sqrt(dx*dx + dy*dy + dz*dz);
+
+		return Math.sqrt(dx * dx + dy * dy + dz * dz);
 	}
-	
-	public Vector3D vectorTo(Point3D other)
-	{
-		return new Vector3D(other.x - this.x,
-				    other.y - this.y,
-				    other.z - this.z);
+
+	public Vector3D vectorTo(Point3D other) {
+		return new Vector3D(other.x - this.x, other.y - this.y, other.z
+				- this.z);
 	}
-	
+
 	@Override
-	public String toString()
-	{
-		return "(" + x, ", " + y + ", " + z + ")";
+	public String toString() {
+		return "(" + x + ", " + y + ", " + z + ")";
 	}
 }
