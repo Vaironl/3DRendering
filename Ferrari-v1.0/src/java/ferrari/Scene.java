6204fda40464b3b2714764891c669bd0f82b5393
Vairon <vairon@live.com>
2014-11-15 21:35:18 -0500

    Fixed typos

--- Ferrari-v1.0/src/java/ferrari/Scene.java
+++ Ferrari-v1.0/src/java/ferrari/Scene.java
@@ -2,37 +2,30 @@ package ferrari;
 
 import java.util.ArrayList;
 
-public class Scene
-{
+public class Scene {
 	private ArrayList<Entity> entities;
-	
-	public Scene()
-	{
+
+	public Scene() {
 		new ArrayList<Entity>();
 	}
-	
-	public ArrayList<Entity> getEntities()
-	{
+
+	public ArrayList<Entity> getEntities() {
 		return entities;
 	}
-	
-	public void setEntities(ArrayList<Entities> ents)
-	{
+
+	public void setEntities(ArrayList<Entity> ents) {
 		entities = ents;
 	}
-	
-	public void add(Entity ent)
-	{
+
+	public void add(Entity ent) {
 		entities.add(ent);
 	}
-	
-	public void remove(Entity ent)
-	{
-		entites.remove(ent);
+
+	public void remove(Entity ent) {
+		entities.remove(ent);
 	}
-	
-	public void clear()
-	{
+
+	public void clear() {
 		entities.clear();
 	}
 }
