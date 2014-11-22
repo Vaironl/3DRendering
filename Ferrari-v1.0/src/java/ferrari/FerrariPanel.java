5f7dc991474e624ba3e02a96e2a0852c3f4f93e7
matiasneal <matias.neal@gmail.com>
2014-11-22 11:00:36 -0500

    Update FerrariPanel.java

--- Ferrari-v1.0/src/java/ferrari/FerrariPanel.java
+++ Ferrari-v1.0/src/java/ferrari/FerrariPanel.java
@@ -1,57 +1,48 @@
 package ferrari;
 
 import javax.swing.JPanel;
-
-import ferrari.geometry.Point3D;
-import ferrari.loader.ModelLoader;
-
 import java.awt.Graphics;
-import java.io.File;
-import java.util.ArrayList;
 
-public class FerrariPanel extends JPanel {
+public class FerrariPanel extends JPanel
+{
 	private Scene scene;
 	private Camera camera;
-	private ModelLoader model;
 
-	private ArrayList<Point3D> vertices = new ArrayList<>();
-
-	public FerrariPanel() {
+	public FerrariPanel()
+	{
 		super();
-
-		File object = new File("src\\teapot.obj.txt");
-
-		model = new ModelLoader(object);
-
-		vertices = model.getVertices();
-
 	}
 
-	public FerrariPanel(Scene scene, Camera camera) {
+	public FerrariPanel(Scene scene, Camera camera)
+	{
 		this();
 		setScene(scene);
 		setCamera(camera);
 	}
 
-	public Scene getScene() {
+	public Scene getScene()
+	{
 		return scene;
 	}
 
-	public Camera getCamera() {
+	public Camera getCamera()
+	{
 		return camera;
 	}
 
-	public void setScene(Scene scene) {
+	public void setScene(Scene scene)
+	{
 		this.scene = scene;
 	}
 
-	public void setCamera(Camera camera) {
+	public void setCamera(Camera camera)
+	{
 		this.camera = camera;
 	}
 
 	@Override
-	protected void paintComponent(Graphics g) {
+	protected void paintComponent(Graphics g)
+	{
 		super.paintComponent(g);
-
 	}
 }
